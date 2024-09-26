833e5502295aecb2c0bd2aea9288275f1119b338
Protagonist <basil2001@hanyang.ac.kr>
2024-09-26 09:58:14 +0000

    주석 처리

--- src/data/customdataloader.py
+++ src/data/customdataloader.py
@@ -2,16 +2,22 @@ import torch.utils.data as data_utils
 
 class CustomDataLoader(data_utils.Dataset):
     # as we already converted to tensor, we can directly return the tensor
-    def __init__(self,x,cons,y,c) -> None:
-        self.x=x
-        self.cons=cons
-        self.y=y
-        self.c=c
+    def __init__(self, x, cons, y, c) -> None:
+        self.x = x
+        self.cons = cons
+        self.y = y
+        self.c = c
         
         #self.emb_x=emb_x
         super().__init__()
+    
+
+
     def __len__(self):
+    
         return len(self.x)
+    
+
     
     def __getitem__(self, index):
 
