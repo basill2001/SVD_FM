5d7e09868c32894b594773256a76caef6a24225e
Protagonist <basil2001@hanyang.ac.kr>
2024-09-26 09:38:00 +0000

    주석 처리

--- src/data/movielens100k.py
+++ src/data/movielens100k.py
@@ -9,12 +9,11 @@ class Movielens100k:
 
     def data_getter(self):
         
-        #train, test loading for each fold
+        # train, test loading for each fold
         self.train = pd.read_csv('dataset/ml-100k/u'+str(self.fold)+'.base',sep='\t',header=None, names=['user_id','movie_id','rating','timestamp'],encoding='latin-1')
         self.test = pd.read_csv('dataset/ml-100k/u'+str(self.fold)+'.test',sep='\t',header=None, names=['user_id','movie_id','rating','timestamp'],encoding='latin-1')
         self.train = self.train.rename(columns={'movie_id':'item_id'})
         self.test = self.test.rename(columns={'movie_id':'item_id'})
-        
         
         movie_info = self.movie_getter()
         user_info = self.user_getter()
@@ -38,39 +37,43 @@ class Movielens100k:
         
         #simple preproccess of user_data
         user_info = pd.read_csv('dataset/ml-100k/u.user',sep='|', names=['age','gender','occupation','zipcode'])
-        # print(user_info)
-        # print(user_info['occupation'].unique())
-        # exit()
         user_info.drop(['zipcode'],axis=1,inplace=True)
+        
         user_info['user_id'] = user_info.index
-        # user_info = pd.get_dummies(columns=['occupation'], data=user_info)
         user_info['gender'] = [1 if i == 'M' else 0 for i in user_info['gender']]
+
         # to discretize age category
         # user_info['age'] = pd.cut(user_info['age'], bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90], labels=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-        user_info['age'] = pd.qcut(user_info['age'], q=10,
-                                   labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        user_info['age'] = pd.qcut(user_info['age'], q=10, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        # occupation categorization
+        # user_info = pd.get_dummies(columns=['occupation'], data=user_info)
+        # occupation_cols = sorted([col for col in user_info.columns if col.startswith('occupation_')])
+       
         user_info['age'] = user_info['age'].astype(int)
         user_info['gender'] = user_info['gender'].astype(int)
         user_info['user_id'] = user_info['user_id'].astype(int)
+        # user_info[occupation_cols] = user_info[occupation_cols].astype(int)
 
-        # reorder columname
-        user_info= user_info[['user_id','age','gender','occupation']]
+        # reorder column name
+        user_info = user_info[['user_id', 'age', 'gender', 'occupation']]
 
         return user_info
     
-    def get_user_item_matrix(self):
 
-        # get useritem matrix
-        train=pd.read_csv('dataset/ml-100k/u'+str(self.fold)+'.base',sep='\t',header=None, names=['user_id','movie_id','rating','timestamp'],encoding='latin-1')
-        useritem_matrix = train.pivot_table(index='user_id',columns='movie_id',values='rating')
+    # 이 부분은 없어도 잘 돌아가는 듯?
+    # def get_user_item_matrix(self):
+
+    #     # get useritem matrix
+    #     train=pd.read_csv('dataset/ml-100k/u'+str(self.fold)+'.base',sep='\t',header=None, names=['user_id','movie_id','rating','timestamp'],encoding='latin-1')
+    #     useritem_matrix = train.pivot_table(index='user_id',columns='movie_id',values='rating')
         
-        print(useritem_matrix)
-        useritem_matrix = useritem_matrix.fillna(0)
-        useritem_matrix = useritem_matrix.astype(float)
-        useritem_matrix[useritem_matrix >= 1] = 1
-        useritem_matrix = useritem_matrix.to_numpy()
-        # x dtype to float
-        useritem_matrix=useritem_matrix.astype(float)  
+    #     print(useritem_matrix)
+    #     useritem_matrix = useritem_matrix.fillna(0)
+    #     useritem_matrix = useritem_matrix.astype(float)
+    #     useritem_matrix[useritem_matrix >= 1] = 1
+    #     useritem_matrix = useritem_matrix.to_numpy()
+    #     # x dtype to float
+    #     useritem_matrix=useritem_matrix.astype(float)  
 
-        return useritem_matrix
+    #     return useritem_matrix
     
